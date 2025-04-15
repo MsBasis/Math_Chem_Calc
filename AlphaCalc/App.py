50a3e29f6896ec24b4a91ce0d15651d24a5baaf5
Mati G <matikgawin@gmail.com>
2025-04-15 16:22:49 +0200

    UPDATE

--- AlphaCalc/App.py
+++ AlphaCalc/App.py
@@ -2,16 +2,17 @@ import tkinter as tk
 
 root = tk.Tk()
 
-# === dimensions ===
+# – – – – – – – – – – – – – – DIMENSIONS – – – – – – – – – – – – – – – – –
 root.title("Simple Calc for math n chem in HS") #title
 root.configure(background="#df7fd5") #background colour
 root.minsize(500,500) #how small it should be
 root.maxsize(500,500)  #how big i can make it
 #root.geometry("300x300+50+50") #the size and shape of window when we first run it 300x300 - size  +50+50 - where it should appear on screen
-# === labels ===
+
+# – – – – – – – – – – – – – – LABELS – – – – – – – – – – – – – – – – –
 #tk.Label(root,text="Calc - short for calculus ").pack()
 
-# === Frames ===
+# – – – – – – – – – – – – – – FRAMES – – – – – – – – – – – – – – – – –
 #Calculations visualizer
 frame = tk.Frame(root, width=450, height=150, bg="#f4b6ed").pack(padx=10,pady=10) #padxy - "distance beetwen other widgets"
 
@@ -25,19 +26,28 @@ def buttons():
         for j in range(3):
             ButtonFrame = tk.Frame(buttons_frame,width=60,height=60,bg="#df7fd5")
             ButtonFrame.place(x=x[j],y=y[i])                                        #if u do it in 1 line it wont actually assign a place to a frame
-            tk.Button(ButtonFrame,text=f"{nr}",bg="#df7fd5",fg="#f4b6ed",).place(x=0,y=0,width=60,height=60)
+            tk.Button(ButtonFrame,text=f"{nr}",bg="#df7fd5",fg="#f4b6ed").place(x=0,y=0,width=60,height=60)
             nr +=1
     ButtonFrame_0 = tk.Frame(buttons_frame,width=60,height=240,bg="#df7fd5")
     ButtonFrame_0.place(x=280,y=210)
-    tk.Button(ButtonFrame_0,text="0",bg="#df7fd5",fg="#f4b6ed",).place(x=0,y=0,width=60,height=240)
+    tk.Button(ButtonFrame_0,text="0",bg="#df7fd5",fg="#f4b6ed").place(x=0,y=0,width=60,height=240)
 buttons()
 
-#calculations buttons
+#Calculations buttons
 calculations_frame = tk.Frame(root,width=100,height=300,bg="#f4b6ed").pack(padx=25,pady=10,side=tk.RIGHT)
 
-ButtonFramePlus = tk.Frame(calculations_frame,width=75,height=60,bg="#df7fd5").place(x=388,y=210)
-ButtonFrameMinus = tk.Frame(calculations_frame,width=75,height=60,bg="#df7fd5").place(x=388,y=300)
-ButtonFrameEqual = tk.Frame(calculations_frame,width=75,height=60,bg="#df7fd5").place(x=388,y=390)
+ButtonFramePlus = tk.Frame(calculations_frame,width=75,height=60,bg="#df7fd5")
+ButtonFramePlus.place(x=388,y=210)
+tk.Button(ButtonFramePlus,text="+",bg="#df7fd5",fg="#f4b6ed").place(x=0,y=0,width=75,height=60)
+
+ButtonFrameMinus = tk.Frame(calculations_frame,width=75,height=60,bg="#df7fd5")
+ButtonFrameMinus.place(x=388,y=300)
+tk.Button(ButtonFrameMinus,text="-",bg="#df7fd5",fg="#f4b6ed").place(x=0,y=0,width=75,height=60)
+
+ButtonFrameEqual = tk.Frame(calculations_frame,width=75,height=60,bg="#df7fd5")
+ButtonFrameEqual.place(x=388,y=390)
+tk.Button(ButtonFrameEqual,text="=",bg="#df7fd5",fg="#f4b6ed").place(x=0,y=0,width=75,height=60)
+
 
 root.mainloop()
 
